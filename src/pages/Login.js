004151e1b843ce7a6f210556f0a91138d58b28cc
Arm Supakorn <arm.supakorn@gmail.com>
2021-07-23 01:15:58 +0700

    Fix SignUp and Login input TextField

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,56 +1,64 @@
 import { Button, FormControl, Paper, TextField } from "@material-ui/core";
 import "./LogIn.css";
 import React from "react";
-import { Redirect } from "react";
+import { useState, Redirect } from "react";
 import { AuthContext } from "../components/Auth";
 import firebaseConfig from "../config";
 
-function Login() {
-  // const handleSubmit = (e) => {
-  //   e.preventDefalut();
-  //   console.log("ok");
-  //   try {
-  //     firebaseConfig
-  //       .auth()
-  //       .signInWithEmailAndPassword(email.value, password.value);
-  //   } catch (error) {
-  //     alert(error);
-  //   }
-  // }
+function LogIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefalut();
+    console.log("ok");
+    try {
+      firebaseConfig
+        .auth()
+        .signInWithEmailAndPassword(email.value, password.value);
+    } catch (error) {
+      alert(error);
+    }
+  };
 
   // const {currentUser} = useContext(AuthContext);
-  // if(currentUser) {
-  //   return <Redirect to="../App" />
-  // }
 
   return (
     <div className="main__login">
       <Paper elevation={10} className="main__paper">
         <FormControl className="main__form" fullWidth>
-          <h1>Sign in</h1>
-          <TextField
-            label="Email"
-            placeholder="Enter email"
-            fullWidth
-            required
-          />
-          <TextField
-            label="Password"
-            placeholder="Enter password"
-            type="password"
-            fullWidth
-            required
-          />
-          <Button type="submit" variant="contained" color="primary" fullWidth>
-            Sign in
-          </Button>
-          <Button type="submit" variant="outlined" color="primary" fullWidth>
-            Sign up
-          </Button>
+          <form onSubmit={handleSubmit}>
+            <h1>Sign in</h1>
+            <TextField
+              label="Email"
+              placeholder="Enter email"
+              fullWidth
+              required
+              // onInput={(e) => {
+              //   setEmail(e.target.value);
+              // }}
+            />
+            <TextField
+              label="Password"
+              placeholder="Enter password"
+              type="password"
+              fullWidth
+              required
+              // onInput={(e) => {
+              //   setPassword(e.target.value);
+              // }}
+            />
+            <Button type="submit" variant="contained" color="primary" fullWidth>
+              Sign in
+            </Button>
+            <Button type="button" variant="outlined" color="primary" fullWidth>
+              Sign up
+            </Button>
+          </form>
         </FormControl>
       </Paper>
     </div>
   );
 }
 
-export default Login;
+export default LogIn;
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,29 +1,29 @@
 import React from "react";
 import { useState } from "react";
 import { Redirect } from "react-router-dom";
+import firebase from "firebase";
 import { Paper, FormControl, TextField, Button } from "@material-ui/core";
 import firebaseConfig from "../config";
 
 function SignUp() {
   const [currentUser, setCurrentUser] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const { email, password } = e.target.elements;
-    console.log("submit");
+    // console.log(email, password);
 
-    // try {
-    //   console.log("test");
-    //   // firebaseConfig
-    //   //   .auth()
-    //   //   .createUserWithEmailAndPassword(email.value, password.value);
-    //   // setCurrentUser(true);
-    // } catch (error) {
-    //   alert(error);
-    // }
+    try {
+      firebaseConfig.auth().createUserWithEmailAndPassword(email, password);
+      setCurrentUser(true);
+    } catch (error) {
+      alert(error);
+    }
   };
 
   if (currentUser) {
+    console.log("already sign up");
     return <Redirect to="../App" />;
   }
 
@@ -31,27 +31,31 @@ function SignUp() {
     <div className="main__login">
       <Paper elevation={10} className="main__paper">
         <FormControl className="main__formControl" fullWidth>
-          <from onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmit}>
             <h1>Sign Up</h1>
             <TextField
               label="Email"
               placeholder="Enter email"
-              autoComplete="email"
               fullWidth
               required
+              onInput={(e) => {
+                setEmail(e.target.value);
+              }}
             />
             <TextField
               label="Password"
               placeholder="Enter password"
               type="password"
-              autoComplete="current-password"
               fullWidth
               required
+              onInput={(e) => {
+                setPassword(e.target.value);
+              }}
             />
             <Button type="submit" variant="contained" color="primary" fullWidth>
               Sign up
             </Button>
-          </from>
+          </form>
         </FormControl>
       </Paper>
     </div>
