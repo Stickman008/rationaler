c5b309958279b3dbe76a95c6cc61af18193c5aaf
Stickman008 <arm.supakorn@gmail.com>
2021-07-22 15:05:51 +0700

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <p>Test Yarn</p>
+      {/* <p>Main page</p> */}
+      <Header />
     </div>
   );
 }
